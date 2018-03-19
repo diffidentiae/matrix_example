7f83516ea109ae0d3a9ac3997cc0e965c0fbe6b0
Aduersarius <31540232+Aduersarius@users.noreply.github.com>
2018-03-19 16:17:07 +0300

    Update matrix.cpp

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -29,3 +29,35 @@ TEST_CASE("reading matrix")
     
     REQUIRE( input == ostream.str() );
 }
+
+TEST_CASE("add matrix")
+{
+    std::string input_1
+    {
+        "3, 3\n"
+        "1 1 1\n"
+        "2 2 2\n"
+        "3 3 3" };
+    std::string input_2
+    {
+        "3, 3\n"
+        "1 1 1\n"
+        "1 1 1\n"
+        "1 1 1" };
+      matrix_t matrix_1, matrix_2;
+    std::istringstream istream_1{ input_1 };
+    std::istringstream istream_2{ input_2 };
+    REQUIRE(matrix1.read(istream_1));
+    REQUIRE(matrix2.read(istream_2));
+    
+    REQUIRE(matrix_1.rows() == 3);
+    REQUIRE(matrix_1.collumns() == 3);
+        
+    REQUIRE(matrix_2.rows() == 3);
+    REQUIRE(matrix_2.collumns() == 3);
+	
+    REQUIRE( matrix_1.collumns() == matrix_2.collumns() && matrix_1.rows() == matrix_2.rows() );
+	  
+    std::ostringstream stream;
+    (matrix_1 + matrix_2).write(stream);
+}
