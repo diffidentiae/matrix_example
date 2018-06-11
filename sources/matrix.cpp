cf45165dfa1bedbdec2c32a81adff7b236787ad9
Aduersarius <31540232+Aduersarius@users.noreply.github.com>
2018-06-11 20:46:15 +0300

    Update matrix.cpp

--- sources/matrix.cpp
+++ sources/matrix.cpp
@@ -189,9 +189,9 @@ std::ostream & matrix_t::write( std::ostream & stream ) const
         stream << '\n';
         for( std::size_t j = 0; j < collumns_; ++j ) {
             stream << elements_[ i ][ j ];
-            if( j != rows_ - 1 || rows_ == 1 ) {
+	    if (rows_ == 1 && j != 1) stream << ' ';
+            if( j != rows_ - 1 ) {
                stream << ' ';
-   	       if (rows_ == 1) break;
             }
         }
     }
